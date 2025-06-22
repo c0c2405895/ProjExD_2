4173030fab08493a99d683e3b63d98a59d412e6a
Renri <c0c2405895@edu.teu.ac.jp>
2025-06-22 18:35:20 +0900

    練習３：画面内外チェック

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,4 @@
-#練習２：爆弾実装
+#練習３：画面内外チェック
 import os
 import random
 import sys
@@ -13,6 +13,19 @@ DELTA = {  # 移動量辞書
     pg.K_RIGHT: (+5, 0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：横方向，縦方向の画面内外判定結果
+    画面内ならTrue，画面外ならFalse
+    """
+    yoko, tate = True, True  # 初期値：画面内
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向の画面外判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom: # 縦方向の画面外判定
+        tate = False
+    return yoko, tate  # 横方向，縦方向の画面内判定結果を返す
 
 
 def main():
@@ -52,8 +65,15 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  # 移動をなかったことにする
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)  # 爆弾の移動
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横方向にはみ出ていたら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出ていたら
+            vy *= -1
         screen.blit(bb_img, bb_rct)  # 爆弾の描画
         pg.display.update()
         tmr += 1
